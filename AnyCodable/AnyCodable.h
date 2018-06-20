49538cbfa0bc906bb3fa893653b70a8ac30ee998
Will Lisac <will.lisac@servicenow.com>
2018-06-20 15:19:15 -0700

    Commit an xcode project to allow Carthage integration
    https://fuller.li/posts/using-swift-package-manager-with-carthage/
    
    https://github.com/Carthage/Carthage/issues/1226

---
+++ AnyCodable/AnyCodable.h
@@ -0,0 +1,19 @@
+//
+//  AnyCodable.h
+//  AnyCodable
+//
+//  Created by Will Lisac on 6/20/18.
+//  Copyright © 2018 Will Lisac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnyCodable.
+FOUNDATION_EXPORT double AnyCodableVersionNumber;
+
+//! Project version string for AnyCodable.
+FOUNDATION_EXPORT const unsigned char AnyCodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyCodable/PublicHeader.h>
+
+
